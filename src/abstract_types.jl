cc9a11fd51dfeb895091b8cff34db900b022b8e0
Dr. Zygmunt L. Szpak <zygmunt.szpak@gmail.com>
2019-05-06 17:37:44 +0930

    Adds Schema types

--- src/abstract_types.jl
+++ src/abstract_types.jl
@@ -3,4 +3,7 @@ abstract type AbstractStatus end
 abstract type AbstractDisplayProperties end
 abstract type AbstractOperation end
 abstract type AbstractImporter <: AbstractOperation end
-abstract type AbstractDialogController end
+abstract type AbstractController end
+abstract type AbstractDialogController <: AbstractController end
+abstract type AbstractSchema end
+abstract type AbstractCSVSchema <: AbstractSchema
